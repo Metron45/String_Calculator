561c68e4ba66bd06e5625ed26c1a32f7bfcd2a6e
Marek Chmielewski <Metron45@gmail.com>
2019-06-29 11:19:24 +0200

    Task3 - Code Cleanup

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,13 +1,6 @@
 
 public class Calculator {
 	
-	public static void main(String[] args) {
-		Calculator calc = new Calculator();
-		System.out.println(calc.Add(""));
-		System.out.println(calc.Add("2,3"));
-		System.out.println(calc.Add("4,5,6,7,7"));
-	}
-	
 	int Add(String numbers) {
 		int result = 0;
 		for(String number : numbers.split(",|\\n")) {
