2c3a161a6776497568751c2dd4ad8cd81c13bbd8
Marek Chmielewski <Metron45@gmail.com>
2019-06-29 12:20:34 +0200

    Task5: Negative Exception

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,21 +1,32 @@
 
 public class Calculator {
 	
-	int Add(String numbers) {
+	int Add(String numbers) throws Exception {
 		int result = 0;
 		String delimiter = ",|\\n";
+		boolean doThrow = false; 
+		String negatives = "";
 		
 		if(numbers.matches("//.+\\n.+")) {
 			delimiter = numbers.split("\\n", 2)[0].substring(2);
 			numbers = numbers.split("\\n", 2)[1];
 		}
-		
+
 		for(String number : numbers.split(delimiter)) {
 			try {
 				result += Integer.valueOf(number);
-			}catch(java.lang.NumberFormatException e) {}
+			}catch(java.lang.NumberFormatException e) {
+				if(number.matches("-[0-9]+")) {
+					doThrow = true;
+					negatives.concat(" " + number);
+				}
+			}
 		}
+		
+		if(doThrow == true) {
+			throw new Exception("negatives not allowed" + negatives);
+		}
+		
 		return result;
 	} 
-	
 }
--- src/Calculator_Tests.java
+++ src/Calculator_Tests.java
@@ -1,50 +1,87 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
 class Calculator_Tests {
 
 	@Test
 	void testAdd() {
 		Calculator calc = new Calculator();
-		assertEquals(0, calc.Add(""));
-		assertEquals(3, calc.Add("3"));
-		assertEquals(4, calc.Add("4"));
-		assertEquals(9, calc.Add("3,6"));
-		assertEquals(12, calc.Add("8,4"));
+		try {
+			assertEquals(0, calc.Add(""));
+			assertEquals(3, calc.Add("3"));
+			assertEquals(4, calc.Add("4"));
+			assertEquals(9, calc.Add("3,6"));
+			assertEquals(12, calc.Add("8,4"));
+		} catch(Exception e) {}
 	}
 
 	@Test
 	void testAddUnknown() {
 		Calculator calc = new Calculator();
-		assertEquals(3, calc.Add("3"));
-		assertEquals(10, calc.Add("4,6"));
-		assertEquals(18, calc.Add("3,6,8,1"));
-		assertEquals(20, calc.Add("8,4,3,2,1,1,1"));
+		try {
+			assertEquals(3, calc.Add("3"));
+			assertEquals(10, calc.Add("4,6"));
+			assertEquals(18, calc.Add("3,6,8,1"));
+			assertEquals(20, calc.Add("8,4,3,2,1,1,1"));
+		} catch(Exception e) {}
 	}
 	
 	@Test
 	void testAddNotNumber() {
 		Calculator calc = new Calculator();
-		assertEquals(3, calc.Add("3,er"));
-		assertEquals(0, calc.Add("a"));
+		try {
+			assertEquals(3, calc.Add("3,er"));
+			assertEquals(0, calc.Add("a"));
+		} catch(Exception e) {}
 	}
 	
 	@Test
 	void testAddNewLine() {
 		Calculator calc = new Calculator();
-		assertEquals(3, calc.Add("3"));
-		assertEquals(10, calc.Add("4\n6"));
-		assertEquals(18, calc.Add("3,6\n8,1"));
-		assertEquals(20, calc.Add("8,4,3\n2,1\n1,1"));
+		try {
+			assertEquals(3, calc.Add("3"));
+			assertEquals(10, calc.Add("4\n6"));
+			assertEquals(18, calc.Add("3,6\n8,1"));
+			assertEquals(20, calc.Add("8,4,3\n2,1\n1,1"));
+		} catch(Exception e) {}
+
 	}
 	
 	@Test
 	void testAddDelimiter() {
 		Calculator calc = new Calculator();
-		assertEquals(3, calc.Add("//k\n3"));
-		assertEquals(10, calc.Add("//;\n4;6"));
-		assertEquals(10, calc.Add("//;s\n4;s6"));
+		try {
+			assertEquals(3, calc.Add("//k\n3"));
+			assertEquals(10, calc.Add("//;\n4;6"));
+			assertEquals(10, calc.Add("//;s\n4;s6"));
+		} catch(Exception e) {}
+	}
+	
+	@Rule
+	public final ExpectedException exception = ExpectedException.none();
+	
+	@Test
+	void testAddNegative() {
+		Calculator calc = new Calculator();
+		exception.expect(Exception.class);
+		try {
+			calc.Add("//k\n-3");
+		} catch(Exception e) {}
+		try {
+			calc.Add("//;s\n-4;s6");
+		} catch(Exception e) {}
+		try {
+			calc.Add("-3");
+		} catch(Exception e) {}
+		try {
+			calc.Add("-4,-6");
+		} catch(Exception e) {}
+		try {
+			calc.Add("-8,4,-3,2,-1,1,1");
+		} catch(Exception e) {}
 	}
 	
 }
